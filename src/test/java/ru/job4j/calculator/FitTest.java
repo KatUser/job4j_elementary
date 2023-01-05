6f8d190bdc8dfe4aeb1e4e65f63b97438c8fbbe1
KatUser <ekaterina.shirokikh@gmail.com>
2023-01-05 15:05:40 +0700

    2. Тесты для задачи "идеальный вес" [#159458]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FitTest {
+
+    @Test
+    public void whenMan200Then115() {
+        short in = 200;
+        double expected = 115;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenMan150Then115() {
+        short in = 150;
+        double expected = 57.5;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenMan165Then74dot75() {
+        short in = 165;
+        double expected = 74.75;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+
+    @Test
+    public void whenWoman200Then103dot5() {
+        short in = 200;
+        double expected = 103.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman150Then46() {
+        short in = 150;
+        double expected = 46;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman165Then63dot25() {
+        short in = 165;
+        double expected = 63.25;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
