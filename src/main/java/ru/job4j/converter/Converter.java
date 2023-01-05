909547c7ed447292d60f4561e60febb45b32a345
KatUser <ekaterina.shirokikh@gmail.com>
2023-01-05 15:58:22 +0700

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -11,15 +11,20 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float inRuble = 120;
-        float expectedDollar = 3;
-        float outRuble = rubleToDollar(inRuble);
-        boolean passedDollarTest = expectedDollar == outRuble;
-        System.out.println("120 rubles are 2. Test result: " + passedDollarTest);
-        float euro = rubleToEuro(140);
         float dollar = rubleToDollar(120);
-        System.out.println("140 rubles are " + euro + " euro.");
+        float euro = rubleToEuro(140);
         System.out.println("120 rubles are " + dollar + " dollar.");
+        System.out.println("140 rubles are " + euro + " euro.");
+        float inRubleToDollar = 120;
+        float inRubleToEuro = 140;
+        float expectedEuro = 2;
+        float expectedDollar = 2;
+        float outRubleToDollar = rubleToDollar(inRubleToDollar);
+        float outRubleToEuro = rubleToEuro(inRubleToEuro);
+        boolean passedDollarTest = expectedDollar == outRubleToDollar;
+        boolean passedEuroTest = expectedEuro == outRubleToEuro;
+        System.out.println("120 rubles are 2 dollar. Test result: " + passedDollarTest);
+        System.out.println("140 rubles are 2 euro. Test result: " + passedEuroTest);
 
     }
 }
