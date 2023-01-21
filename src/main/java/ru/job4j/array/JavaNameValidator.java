35a10060da68013bf9b5c67658b422539c9caeca
KatUser <ekaterina.shirokikh@gmail.com>
2023-01-21 11:41:58 +0700

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,24 +2,15 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean nameIs = false;
-        if (!name.isEmpty()) {
-            int[] numbers = new int[name.length()];
-            for (int i = 0; i < name.length(); i++) {
-                int code = name.codePointAt(i);
-                numbers[i] = code;
-                if (!isLowerLatinLetter(numbers[0])) {
-                    break;
-                }
-                for (int index = 1; index < numbers.length; index++) {
-                    if (isUpperLatinLetter(numbers[index])
-                            || isLowerLatinLetter(numbers[index])
-                            || isSpecialSymbol(numbers[index])
-                            || Character.isDigit(numbers[index])) {
-                        nameIs = true;
-                    }
-
-                }
+        boolean nameIs = !name.isEmpty() && isLowerLatinLetter(name.charAt(0));
+        for (int i = 1; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (!isSpecialSymbol(code)
+                    && !isUpperLatinLetter(code)
+                    && !isLowerLatinLetter(code)
+                    && !isNumber(code)) {
+                nameIs = false;
+                break;
             }
         }
         return nameIs;
@@ -30,10 +21,14 @@ public class JavaNameValidator {
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return ((code >= 65) && (code <= 90));
+        return Character.isUpperCase((char) code);
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return ((code >= 97) && (code <= 122));
+        return Character.isLowerCase((char) code);
+    }
+
+    public static boolean isNumber(int code) {
+        return Character.isDigit((char) code);
     }
 }
